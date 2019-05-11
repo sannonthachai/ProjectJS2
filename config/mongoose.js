6c1a85df95ac4ebd01e76ef9cb53393b83fd2f51
sannonthachai <sannonthachai@gmail.com>
2019-05-11 09:34:08 +0700

    add log & alter deprecate

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,8 @@ const config = require('./config');
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    let db = mongoose.connect(config.mongoUri);
+    let db = mongoose.connect(config.mongoUri,{ useNewUrlParser: true });
+    console.log("Database is connect!");
 
     return db;
 };
